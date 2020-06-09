7463cf9bd12ff4fd217fe2e57ac91fc0934d4435
alexandr paschenko <alexandr.paschenko@succraft.com>
2020-06-09 17:09:48 +0300

    Added default route

--- src/app/index.js
+++ src/app/index.js
@@ -3,10 +3,12 @@ const app = express();
 const bodyParser = require("body-parser");
 
 const recognize = require('../routes/recognize');
+const defaultRoute = require('../routes/defaultRoute');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('/', defaultRoute)
 app.use('/recognize', recognize);
 app.use((err, req, res, next) => {
   res.status(err.code || 400).json({
